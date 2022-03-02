19a2762af9d118db4abcfbeb0c8a56155647f14a
JungGyeongmi <gm950715@gmail.com>
2022-03-02 09:16:10 +0900

    Initial commit

---
+++ vueex2/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <img src="./assets/커피춘배.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+
+<script>
+
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: '나는 민지쨩!'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #dfb8f3;
+}
+</style>
---
+++ vueex2/src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+// import Code from './code.vue'
+
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
